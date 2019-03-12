aca735f85d2bc2a75f2fd92cf3538c6a9fad217c
Emmanuel Pinault <emmanuel.pinault@outreach.io>
2019-03-12 12:52:11 -0700

    Switch webmock to call disable_net_connect

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'webmock/rspec'
 # Load all files from `spec/support`
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
 
-WebMock.disable!
+WebMock.disable_net_connect!
 
 RSpec.configure do |config|
   if config.files_to_run.one?
